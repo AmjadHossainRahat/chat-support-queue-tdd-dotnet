067ec72249772af513d3d5c708fa07c25810f70c
MD. Amjad Hossain <amjad.hossain.rahat@gmail.com>
2026-03-12 02:41:08 +0600

    refactor(domain): compute agents capacity using multiplier policy

--- src/SupportChat.Domain/Agents/Agent.cs
+++ src/SupportChat.Domain/Agents/Agent.cs
@@ -2,6 +2,9 @@
 {
     public class Agent
     {
+        // TODO: make it configurable
+        private const int BaseChatCapacity = 10;
+
         public Guid Id { get; }
         public Seniority Seniority { get; }
 
@@ -13,7 +16,17 @@
 
         public int GetMaxConcurrentChats()
         {
-            throw new NotImplementedException();
+            // TODO: make it configurable
+            var multiplier = Seniority switch
+            {
+                Seniority.Junior => 0.4,
+                Seniority.Mid => 0.6,
+                Seniority.Senior => 0.8,
+                Seniority.TeamLead => 0.5,
+                _ => throw new ArgumentOutOfRangeException(nameof(Seniority), Seniority, null)
+            };
+
+            return (int)Math.Floor(BaseChatCapacity * multiplier);
         }
     }
 }

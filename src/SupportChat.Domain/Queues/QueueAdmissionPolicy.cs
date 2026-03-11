cbc5e1319efe19cd78d3c9661d99e9cb434e8e31
MD. Amjad Hossain <amjad.hossain.rahat@gmail.com>
2026-03-12 05:18:56 +0600

    feat(domain): implemented queue admission policy

--- src/SupportChat.Domain/Queues/QueueAdmissionPolicy.cs
+++ src/SupportChat.Domain/Queues/QueueAdmissionPolicy.cs
@@ -11,6 +11,27 @@ public class QueueAdmissionPolicy
         int currentOverflowQueueCount,
         bool isWithinOfficeHours)
     {
-        throw new NotImplementedException();
+        var mainQueueLimit = mainTeam.GetQueueLimit();
+
+        // Main queue has capacity
+        if (currentMainQueueCount < mainQueueLimit)
+        {
+            return QueueAdmissionResult.MainQueue;
+        }
+
+        // Main queue full
+        if (!isWithinOfficeHours)
+        {
+            return QueueAdmissionResult.Rejected;
+        }
+
+        var overflowQueueLimit = overflowTeam.GetQueueLimit();
+
+        if (currentOverflowQueueCount < overflowQueueLimit)
+        {
+            return QueueAdmissionResult.OverflowQueue;
+        }
+
+        return QueueAdmissionResult.Rejected;
     }
 }

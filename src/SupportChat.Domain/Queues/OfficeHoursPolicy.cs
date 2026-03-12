6bcda7576797cf60b8129cc0328068281c2422ab
MD. Amjad Hossain <amjad.hossain.rahat@gmail.com>
2026-03-12 17:52:54 +0600

    test(domain): added test cases for office hour policy

---
+++ src/SupportChat.Domain/Queues/OfficeHoursPolicy.cs
@@ -0,0 +1,17 @@
+﻿namespace SupportChat.Domain.Queues;
+
+public class OfficeHoursPolicy
+{
+    public TimeOnly StartTime { get; }
+    public TimeOnly EndTime { get; }
+
+    public OfficeHoursPolicy(TimeOnly startTime, TimeOnly endTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsWithinOfficeHours(TimeOnly currentTime)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/SupportChat.UnitTests/Queues/OfficeHoursPolicyTests.cs
@@ -0,0 +1,23 @@
+﻿using SupportChat.Domain.Queues;
+
+namespace SupportChat.UnitTests.Queues;
+
+public class OfficeHoursPolicyTests
+{
+    private readonly OfficeHoursPolicy _policy = new(new TimeOnly(9, 0), new TimeOnly(17, 0));
+
+    [TestCase("09:00", true)]
+    [TestCase("12:30", true)]
+    [TestCase("16:59", true)]
+    [TestCase("08:59", false)]
+    [TestCase("17:00", false)]
+    [TestCase("22:00", false)]
+    public void Should_correctly_identify_if_time_is_within_office_hours(string timeText, bool expected)
+    {
+        var time = TimeOnly.Parse(timeText);
+
+        var result = _policy.IsWithinOfficeHours(time);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}

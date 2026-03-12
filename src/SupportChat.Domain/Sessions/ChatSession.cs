dd938cde8101f5e5b0facf67bb55725029f3f21e
MD. Amjad Hossain <amjad.hossain.rahat@gmail.com>
2026-03-12 21:55:37 +0600

    test(domain): added entity and test cases for Chat Session

---
+++ src/SupportChat.Domain/Sessions/ChatSession.cs
@@ -0,0 +1,40 @@
+﻿namespace SupportChat.Domain.Sessions;
+
+public class ChatSession
+{
+    public Guid Id { get; }
+    public SessionStatus Status { get; private set; }
+    public DateTime CreatedAtUtc { get; }
+    public DateTime? LastPolledAtUtc { get; private set; }
+
+    public ChatSession(Guid id, DateTime createdAtUtc)
+    {
+        Id = id;
+        CreatedAtUtc = createdAtUtc;
+    }
+
+    public void RegisterPoll(DateTime polledAtUtc)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkAssigned()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkInactive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkRejected()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MarkCompleted()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ tests/SupportChat.UnitTests/Sessions/ChatSessionTests.cs
@@ -0,0 +1,53 @@
+﻿using SupportChat.Domain.Sessions;
+
+namespace SupportChat.UnitTests.Sessions;
+
+public class ChatSessionTests
+{
+    [Test]
+    public void New_session_should_start_in_queued_status()
+    {
+        var session = new ChatSession(
+            Guid.NewGuid(),
+            new DateTime(2026, 3, 12, 10, 0, 0, DateTimeKind.Utc));
+
+        Assert.That(session.Status, Is.EqualTo(SessionStatus.Queued));
+    }
+
+    [Test]
+    public void Register_poll_should_update_last_polled_time()
+    {
+        var createdAt = new DateTime(2026, 3, 12, 10, 0, 0, DateTimeKind.Utc);
+        var polledAt = createdAt.AddSeconds(1);
+
+        var session = new ChatSession(Guid.NewGuid(), createdAt);
+
+        session.RegisterPoll(polledAt);
+
+        Assert.That(session.LastPolledAtUtc, Is.EqualTo(polledAt));
+    }
+
+    [Test]
+    public void Mark_assigned_should_set_status_to_assigned()
+    {
+        var session = new ChatSession(
+            Guid.NewGuid(),
+            new DateTime(2026, 3, 12, 10, 0, 0, DateTimeKind.Utc));
+
+        session.MarkAssigned();
+
+        Assert.That(session.Status, Is.EqualTo(SessionStatus.Assigned));
+    }
+
+    [Test]
+    public void Mark_inactive_should_set_status_to_inactive()
+    {
+        var session = new ChatSession(
+            Guid.NewGuid(),
+            new DateTime(2026, 3, 12, 10, 0, 0, DateTimeKind.Utc));
+
+        session.MarkInactive();
+
+        Assert.That(session.Status, Is.EqualTo(SessionStatus.Inactive));
+    }
+}

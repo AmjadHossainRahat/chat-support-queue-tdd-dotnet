4bb38c0ff6a6896f40cfd856ee3da4d7dfbdf8a0
MD. Amjad Hossain <amjad.hossain.rahat@gmail.com>
2026-03-14 23:37:58 +0600

    feat(worker): added worker host bootstrap for queued session assignment

--- src/SupportChat.Worker/Program.cs
+++ src/SupportChat.Worker/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello from Worker!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SupportChat.Application.Abstractions;
+using SupportChat.Application.Assignments;
+using SupportChat.Domain.Assignments;
+using SupportChat.Infrastructure.Persistence;
+using SupportChat.Infrastructure.Providers;
+using SupportChat.Worker.Assignment;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddSingleton<IChatSessionRepository, InMemoryChatSessionRepository>();
+builder.Services.AddSingleton<IAgentProvider, InMemoryAgentProvider>();
+
+builder.Services.AddSingleton<AssignmentPolicy>();
+builder.Services.AddSingleton<AssignWaitingSessionUseCase>();
+builder.Services.AddSingleton<AssignNextQueuedSessionUseCase>();
+builder.Services.AddSingleton<QueuedSessionAssignmentProcessor>();
+
+builder.Services.AddHostedService<QueuedSessionAssignmentWorker>();
+
+var host = builder.Build();
+host.Run();
